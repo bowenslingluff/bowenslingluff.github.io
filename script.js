1da7b7e7a7361953a844b59a2d648ccb6aff63aa
Bowen Slingluff <bcslingluff@Bowens-MacBook-Pro.local>
2024-04-13 23:31:30 -0400

    added js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const modeToggle = document.querySelector('.mode');
+    const modeToggle = document.querySelector('.theme');
     const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
     if (currentTheme) {
         document.documentElement.setAttribute('data-theme', currentTheme);
 
         if (currentTheme === 'dark') {
-            modeToggle.innerHTML = '<i class="bi bi-moon-fill"></i>';
+            modeToggle.innerHTML = '<input class="input" checked="checked" type="checkbox"><i class="bi bi-moon-fill"></i>';
         }
     }
 
@@ -14,11 +14,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
         let theme = 'light';
         if (document.documentElement.getAttribute('data-theme') === 'light') {
             theme = 'dark';
-            modeToggle.innerHTML = '<i class="bi bi-moon-fill"></i>';
+            modeToggle.innerHTML = '<input class="input" checked="checked" type="checkbox"><i class="bi bi-moon-fill"></i>';
         } else {
-            modeToggle.innerHTML = '<i class="bi bi-sun"></i>';
+            modeToggle.innerHTML = '<input class="input" checked="checked" type="checkbox"><i class="bi bi-sun"></i>';
         }
         document.documentElement.setAttribute('data-theme', theme);
         localStorage.setItem('theme', theme);
     });
 });
+
+document.addEventListener('scroll', function() {
+    var scrollElement = document.querySelector('.scroll');
+    if (window.scrollY > 20) { // Adjust the value based on your needs
+      scrollElement.style.opacity = '0'; // Fade out the arrows when scrolling down
+    } else {
+      scrollElement.style.opacity = '1'; // Fade in the arrows when at the top
+    }
+  });
+  
+  
